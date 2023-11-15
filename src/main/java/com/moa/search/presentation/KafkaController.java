e535062b32d4906045477227c419612220376a10
awdfaf <awdfaf@kakao.com>
2023-11-15 14:26:32 +0900

    kafka test

---
+++ src/main/java/com/moa/search/presentation/KafkaController.java
@@ -0,0 +1,25 @@
+package com.moa.search.presentation;
+
+import com.moa.search.application.KafkaProducer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/kafka")
+public class KafkaController {
+
+    private final KafkaProducer producer;
+
+    @Autowired
+    KafkaController(KafkaProducer producer) {
+        this.producer = producer;
+    }
+
+    @GetMapping
+    public String sendMessage() {
+        this.producer.sendMessage("test");
+        return "SUCCESS";
+    }
+}
--- src/main/java/com/moa/search/presentation/MeetingController.java
+++ src/main/java/com/moa/search/presentation/MeetingController.java
@@ -32,4 +32,5 @@ public class MeetingController {
         return ResponseEntity.ok(meetings);
     }
 
+
 }
