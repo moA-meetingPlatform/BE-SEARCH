c68cffabb4cf14d21b5b751d147ea7fe1ea6881e
awdfaf <awdfaf@kakao.com>
2023-11-16 12:53:41 +0900

    test

--- src/main/java/com/moa/search/infrastructure/kafka/KafkaConsumer.java
+++ src/main/java/com/moa/search/infrastructure/kafka/KafkaConsumer.java
@@ -8,10 +8,14 @@ import java.io.IOException;
 @Service
 public class KafkaConsumer {
 
-    @KafkaListener(topics = "local-test", groupId = "test-group")
+    @KafkaListener(topics = "logstash-test", groupId = "test-group")
     public void consume(String message) throws IOException {
         System.out.println(String.format("Consumed message : %s", message));
     }
-
+    @KafkaListener(topics = "local-test", groupId = "your-consumer-group")
+    public void listen(String message) {
+        System.out.println("Received message in group your-consumer-group: " + message);
+        // 여기에서 추가적인 처리를 할 수 있습니다.
+    }
 
 }
--- src/main/java/com/moa/search/infrastructure/kafka/KafkaProducer.java
+++ src/main/java/com/moa/search/infrastructure/kafka/KafkaProducer.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class KafkaProducer {
-    private static final String TOPIC = "local-test";
+    private static final String TOPIC = "logstash-test";
     private final KafkaTemplate<String, String> kafkaTemplate;
 
     @Autowired
